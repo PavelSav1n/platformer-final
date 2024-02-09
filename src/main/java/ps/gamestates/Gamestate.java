8a2bb38a6d932a719dd0dd5ad935f4dc68619f06
Pavel <savin.pavel.a@gmail.com>
2024-02-09 18:34:02 +0400

    Final Version of a Platformer Game

---
+++ src/main/java/ps/gamestates/Gamestate.java
@@ -0,0 +1,9 @@
+package ps.gamestates;
+
+public enum Gamestate {
+
+	PLAYING, MENU, OPTIONS, QUIT, CREDITS;
+
+	public static Gamestate state = MENU;
+
+}
---
+++ src/main/java/ps/main/MainClass.java
@@ -0,0 +1,9 @@
+package ps.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		new Game();
+	}
+
+}
